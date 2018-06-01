162aad66896203c2de9761d4dac6e0bc5d1d95de
Paul <paulsblackham@sky.com>
2018-06-01 13:53:12 +0100

    Completed Lecture 49
    Adjustable thrust and rotation

--- 3_Project_Boost/Assets/Scripts/Rocket.cs
+++ 3_Project_Boost/Assets/Scripts/Rocket.cs
@@ -6,23 +6,28 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource mainEngineAudio;
+    [SerializeField] float rcsThrust = 200f;
+    [SerializeField] float mainThrust = 1300f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         mainEngineAudio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Rotate();
+        Thrust();
+    }
 
-    private void ProcessInput()
+    private void Thrust()
     {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!mainEngineAudio.isPlaying)
             {
                 mainEngineAudio.Play();
@@ -32,14 +37,23 @@ public class Rocket : MonoBehaviour {
         {
             mainEngineAudio.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation - fixes bug of unstoppable spin
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
