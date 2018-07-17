4ad30acec98b5b4161e8078dbb5e93adbc043cde
Paul <paulsblackham@sky.com>
2018-07-17 12:30:45 +0100

    Completed lecture 106

--- 4_Argon_Assault/Assets/Scripts/Enemy.cs
+++ 4_Argon_Assault/Assets/Scripts/Enemy.cs
@@ -1,21 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Start ()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
