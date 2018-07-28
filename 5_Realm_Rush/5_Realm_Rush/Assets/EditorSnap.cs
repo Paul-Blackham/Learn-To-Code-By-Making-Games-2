3fdb418b5577f838737151e6eacf3eec84196e37
Paul <paulsblackham@sky.com>
2018-07-29 00:21:49 +0100

    Completed lecture 123
    Begun project 5, Realm Rush

---
+++ 5_Realm_Rush/5_Realm_Rush/Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+    [SerializeField] [Range(1, 100)] int gridSize = 10;
+
+	
+	void Update () {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+	}
+}
