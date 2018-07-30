f6acba2525660b43fde436c7c870f1364b448e26
Paul <paulsblackham@sky.com>
2018-07-30 09:58:13 +0100

    Completed Lecture 124
    Now has text on top of box that states it's grid coordinates

--- 5_Realm_Rush/5_Realm_Rush/Assets/Scripts/CubeEditor.cs
+++ 5_Realm_Rush/5_Realm_Rush/Assets/Scripts/CubeEditor.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField] [Range(1, 100)] int gridSize = 10;
 
-	
-	void Update () {
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = "X" + "," + "Z";
+    }
+
+
+    void Update () {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 	}
